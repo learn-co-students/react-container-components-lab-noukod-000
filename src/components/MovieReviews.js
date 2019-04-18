7d436ae6e59ea7a63e3b9e8213ebc042425caa6c
Keren-Happuc Fleurimond <keren@noukod.com>
2019-04-18 15:36:36 +0000

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,22 @@
 // Code MovieReviews Here
+import React from 'react'
+
+const Review = ({title, summary}) => {
+  return (
+    <div key={title} className="review">
+      <hr></hr>
+      <h2>{title}</h2>
+      <h5><i>{summary}</i></h5>
+    </div>
+  )
+}
+
+const MovieReviews = ({reviews}) => (
+  <div className="review-list">{reviews.map(Review)}</div>
+)
+
+MovieReviews.defaultProps = {
+  reviews: []
+}
+
+export default MovieReviews
